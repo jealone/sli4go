67a67a1a25164cfb1cc3f98623da12acbde9f303
Arthur Guo <jealone@gmail.com>
2019-11-19 19:54:20 +0800

    fix(calldepth):change to 3 level

--- standard.go
+++ standard.go
@@ -2,7 +2,6 @@ package sli4go
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
@@ -10,85 +9,98 @@ import (
 type StandardLogger struct {
 }
 
-func (l *StandardLogger) Writer() io.Writer {
-	return log.Writer()
+var (
+	calldepth = 3
+)
+
+// SetCallDepth 设置调用层
+func SetCallDepth(depth int) {
+	calldepth = depth
 }
 
 func (l *StandardLogger) Fatal(v ...interface{}) {
-	log.Fatal(v...)
+	log.Output(calldepth, fmt.Sprint(v...))
+	os.Exit(1)
 }
 func (l *StandardLogger) Fatalf(format string, v ...interface{}) {
-	log.Output(3, fmt.Sprintf(format, v...))
+	log.Output(calldepth, fmt.Sprintf(format, v...))
 	os.Exit(1)
-	//log.Fatalf(format, v...)
 }
 func (l *StandardLogger) Fatalln(v ...interface{}) {
-	log.Output(3, fmt.Sprintln(v...))
+	log.Output(calldepth, fmt.Sprintln(v...))
 	os.Exit(1)
-	//log.Fatalln(v...)
 }
 func (l *StandardLogger) Panic(v ...interface{}) {
-	log.Panic(v...)
+	s := fmt.Sprint(v...)
+	log.Output(calldepth, s)
+	panic(s)
 }
 func (l *StandardLogger) Panicf(format string, v ...interface{}) {
-	log.Panicf(format, v...)
+	s := fmt.Sprintf(format, v...)
+	log.Output(calldepth, s)
+	panic(s)
 }
 func (l *StandardLogger) Panicln(v ...interface{}) {
-	log.Panicln(v...)
+	s := fmt.Sprintln(v...)
+	log.Output(calldepth, s)
+	panic(s)
 }
 
 func (l *StandardLogger) Print(v ...interface{}) {
-	log.Print(v...)
+	log.Output(calldepth, fmt.Sprint(v...))
 }
 func (l *StandardLogger) Printf(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	log.Output(calldepth, fmt.Sprintf(format, v...))
 }
 func (l *StandardLogger) Println(v ...interface{}) {
-	log.Println(v...)
+	log.Output(calldepth, fmt.Sprintln(v...))
 }
 
 func (l *StandardLogger) Trace(v ...interface{}) {
-	log.Print(v...)
+	log.Output(calldepth, fmt.Sprint(v...))
 }
 func (l *StandardLogger) Tracef(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	log.Output(calldepth, fmt.Sprintf(format, v...))
 }
 func (l *StandardLogger) Traceln(v ...interface{}) {
-	log.Println(v...)
+	log.Output(calldepth, fmt.Sprintln(v...))
 }
 func (l *StandardLogger) Debug(v ...interface{}) {
-	log.Print(v...)
+	log.Output(calldepth, fmt.Sprint(v...))
 }
 func (l *StandardLogger) Debugf(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	log.Output(calldepth, fmt.Sprintf(format, v...))
 }
 func (l *StandardLogger) Debugln(v ...interface{}) {
-	log.Println(v...)
+	log.Output(calldepth, fmt.Sprintln(v...))
 }
 func (l *StandardLogger) Info(v ...interface{}) {
-	log.Print(v...)
+	log.Output(calldepth, fmt.Sprint(v...))
+
 }
 func (l *StandardLogger) Infof(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	log.Output(calldepth, fmt.Sprintln(v...))
 }
 func (l *StandardLogger) Infoln(v ...interface{}) {
-	log.Println(v...)
+	log.Output(calldepth, fmt.Sprintln(v...))
+
 }
 func (l *StandardLogger) Warn(v ...interface{}) {
-	log.Print(v...)
+	log.Output(calldepth, fmt.Sprint(v...))
 }
 func (l *StandardLogger) Warnf(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	log.Output(calldepth, fmt.Sprintf(format, v...))
 }
 func (l *StandardLogger) Warnln(v ...interface{}) {
-	log.Println(v...)
+	log.Output(calldepth, fmt.Sprintln(v...))
+
 }
 func (l *StandardLogger) Error(v ...interface{}) {
-	log.Print(v...)
+	log.Output(calldepth, fmt.Sprint(v...))
 }
 func (l *StandardLogger) Errorf(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	log.Output(calldepth, fmt.Sprintf(format, v...))
 }
 func (l *StandardLogger) Errorln(v ...interface{}) {
-	log.Println(v...)
+	log.Output(calldepth, fmt.Sprintln(v...))
 }
